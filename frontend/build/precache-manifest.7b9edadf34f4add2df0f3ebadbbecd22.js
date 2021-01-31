eb983d04a98a584234bdd257304943b7924ccdd6
Jacob Hite <1085644+jrhite@users.noreply.github.com>
2021-01-31 20:08:32 +0700

    adding in build dir

---
+++ frontend/build/precache-manifest.7b9edadf34f4add2df0f3ebadbbecd22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326d23bd8f64c98f668ff080d3eba8d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1e3dbbce0bba2868fab",
+    "url": "/static/js/2.a0bb07f2.chunk.js"
+  },
+  {
+    "revision": "aebf116bd433f76052fafcd18fc9cbe9",
+    "url": "/static/js/2.a0bb07f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21753bf6c7312c54bf25",
+    "url": "/static/js/main.f7e2b875.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
