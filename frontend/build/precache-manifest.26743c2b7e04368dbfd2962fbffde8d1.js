c4d913af42c77865c06b775bb4208df0768b2628
Jacob Hite <1085644+jrhite@users.noreply.github.com>
2021-02-02 12:47:29 +0700

    prepare for rebase to master

---
+++ frontend/build/precache-manifest.26743c2b7e04368dbfd2962fbffde8d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52740bf2367a7fca480de704dac0cbed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1e3dbbce0bba2868fab",
+    "url": "/static/js/2.a0bb07f2.chunk.js"
+  },
+  {
+    "revision": "aebf116bd433f76052fafcd18fc9cbe9",
+    "url": "/static/js/2.a0bb07f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11042550a714e06dfc6a",
+    "url": "/static/js/main.25411d5a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bazaarfinance/precache-manifest.f8e5e35bb40438f17d525f38508209fd.js"
+  "/precache-manifest.26743c2b7e04368dbfd2962fbffde8d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bazaarfinance/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
